cc4feb1826493ca225683a2c5d94ce98491e7ce7
Pheerie <piri.komp@gmail.com>
2021-03-02 14:45:43 +0300

    23.7.1

--- app/src/main/java/com/example/futuresomething/MainActivity.kt
+++ app/src/main/java/com/example/futuresomething/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var backPressed = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,25 +21,7 @@ class MainActivity : AppCompatActivity() {
         iniFragment()
     }
 
-    fun launchDetailsFragment(film: Film) {
-        //Создаем "посылку"
-        val bundle = Bundle()
-        //Кладем наш фильм в "посылку"
-        bundle.putParcelable("film", film)
-        //Кладем фрагмент с деталями в перменную
-        val fragment = DetailsFragment()
-        //Прикрепляем нашу "посылку" к фрагменту
-        fragment.arguments = bundle
-
-        //Запускаем фрагмент
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_placeholder, fragment)
-            .addToBackStack(null)
-            .commit()
-    }
-
-    fun iniFragment() {
+    private fun iniFragment() {
         supportFragmentManager
             .beginTransaction()
             .add(R.id.fragment_placeholder, HomeFragment())
@@ -43,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
-    fun initNavigation() {
+    private fun initNavigation() {
         val snackbarFavorites = Snackbar.make(main, R.string.favorites, Snackbar.LENGTH_SHORT)
         val snackbarViewLater = Snackbar.make(main, R.string.view_later, Snackbar.LENGTH_SHORT)
 
@@ -67,7 +53,7 @@ class MainActivity : AppCompatActivity() {
                     }
                     true
                 }
-                R.id.watch_later     -> {
+                R.id.watch_later -> {
                     snackbarViewLater.show()
                     snackbarViewLater.setAction("Ok") {
                         snackbarFavorites.dismiss()
@@ -81,5 +67,45 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount == 1) {
+            doubleTapToExit()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun doubleTapToExit() {
+        if (backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            finish()
+        } else {
+            Toast.makeText(this, "Double tap for exit", Toast.LENGTH_SHORT).show()
+        }
+
+        backPressed = System.currentTimeMillis()
+    }
+
+    fun launchDetailsFragment(film: Film) {
+        //Создаем "посылку"
+        val bundle = Bundle()
+        //Кладем наш фильм в "посылку"
+        bundle.putParcelable("film", film)
+        //Кладем фрагмент с деталями в перменную
+        val fragment = DetailsFragment()
+        //Прикрепляем нашу "посылку" к фрагменту
+        fragment.arguments = bundle
+
+        //Запускаем фрагмент
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_placeholder, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+        const val TIME_INTERVAL = 2000
     }
 }
