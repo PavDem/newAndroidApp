a8b886199f39b8139bfb9f5e4bcea01aee6b2cfe
Pheerie <piri.komp@gmail.com>
2021-03-25 20:29:40 +0300

    final changes

--- app/src/main/java/com/example/futuresomething/HomeFragment.kt
+++ app/src/main/java/com/example/futuresomething/HomeFragment.kt
@@ -16,13 +16,13 @@ HomeFragment : Fragment() {
     private lateinit var filmsAdapter: FilmListRecyclerAdapter
     private val filmsDataBase = listOf(
         Film(0, "Willy Wonka", R.drawable.willy_wonka, "Willy Wonka is extraordinary. He's a chocolate-making genius who relishes nonsense. He can't abide ugliness in factories. And he likes to make mischief, even if that means talking to the President of the United States whilst pretending to be a man from Mars, as he does in Charlie and the Great Glass Elevator."),
-        Film(1, "Film title", R.drawable.joker, "This should be a description"),
-        Film(3, "Film title", R.drawable.interstellar, "This should be a description"),
-        Film(4, "Film title", R.drawable.the_thing, "This should be a description"),
-        Film(5, "Film title", R.drawable.the_godfather, "This should be a description"),
-        Film(6, "Film title", R.drawable.akira, "This should be a description"),
-        Film(7, "Film title", R.drawable.the_shawshank_redemption, "This should be a description"),
-        Film(8, "Film title", R.drawable.deerskin, "This should be a description")
+        Film(1, "JOKER", R.drawable.joker, "This should be a description"),
+        Film(3, "INTERSTELLAR", R.drawable.interstellar, "This should be a description"),
+        Film(4, "The Thing", R.drawable.the_thing, "This should be a description"),
+        Film(5, "The Godfather", R.drawable.the_godfather, "This should be a description"),
+        Film(6, "Akira", R.drawable.akira, "This should be a description"),
+        Film(7, "The Shawshank Redemption", R.drawable.the_shawshank_redemption, "This should be a description"),
+        Film(8, "Deerskin", R.drawable.deerskin, "This should be a description")
 
     )
 
