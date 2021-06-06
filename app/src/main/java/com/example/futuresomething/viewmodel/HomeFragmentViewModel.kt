05fc47f8fbb1dd01a6906140987deb7e1429c18a
Pheerie <piri.komp@gmail.com>
2021-06-06 10:03:12 +0300

    module 34, koin version

--- app/src/main/java/com/example/futuresomething/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/futuresomething/viewmodel/HomeFragmentViewModel.kt
@@ -2,14 +2,16 @@ package com.example.futuresomething.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.futuresomething.App
 import com.example.futuresomething.domain.Film
 import com.example.futuresomething.domain.Interactor
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
-    val filmsListLiveData:  MutableLiveData<List<Film>> = MutableLiveData()
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
+    val filmsListLiveData: MutableLiveData<List<Film>> = MutableLiveData()
+
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
+    private val interactor: Interactor by inject()
 
     init {
         interactor.getFilmsFromApi(1, object : ApiCallback {
